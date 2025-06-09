08ddf931f60755162287598255653806174eff20
Alexandre Francineau <francineau.alexandre@gmail.com>
2025-06-09 19:56:05 +0200

    first commit

---
+++ w1/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = ({name}) => {
+  return (
+     <section className='w-96 h-64 bg-gray-300 mt-12 flex justify-center items-center text-gray-500 border rounded-xl' >
+        <p className='text-xl font-bold'>My name is {name} </p>
+    </section>   
+    
+  )
+}
+
+export default Card
---
+++ w1/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='w-full fixed top-0 flex justify-center py-8 backdrop-blur-3xl'>
+        <h1 className="text-3xl font-bold text-gray-500">
+            Welcome to my landing page
+        </h1>
+    </header>
+  )
+}
+
+export default Header
