9bcecbd6a95f2b331afec0ea2a53b61f215641ae
Alexandre Francineau <francineau.alexandre@gmail.com>
2025-06-09 20:20:50 +0200

    fix text size

--- w1/src/components/Header.tsx
+++ w1/src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header className='w-full fixed top-0 flex justify-center py-8 backdrop-blur-3xl'>
-        <h1 className="text-3xl font-bold text-gray-500">
+        <h1 className="text-xl p-4 lg:text-3xl font-bold text-gray-500">
             Welcome to my landing page
         </h1>
     </header>
